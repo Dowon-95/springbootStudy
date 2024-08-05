c00731b012e796498ae4f5072b90db3fab0b8f6d
Dowon <tnstjs1919@gmail.com>
2024-08-05 10:56:30 +0900

    src/test/java/com/example/FirstprojectStudy/FirstprojectStudyApplicationTests.j

---
+++ src/main/java/com/example/FirstprojectStudy/FirstprojectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstprojectStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstprojectStudy/FirstprojectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstprojectStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
