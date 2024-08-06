6f57a3a7810c2c6ee387e545cde397e1fc51de26
Dowon <tnstjs1919@gmail.com>
2024-08-06 10:09:03 +0900

    컨트롤러생성 및 폼데이터 전송

---
+++ src/main/java/com/example/FirstprojectStudy/dto/ArticleForm.java
@@ -0,0 +1,19 @@
+package com.example.FirstprojectStudy.dto;
+
+public class ArticleForm {
+    private String title;
+    private String contents;
+
+    public ArticleForm(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
+    }
+}
